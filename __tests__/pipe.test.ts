0df3988b50b60131ccefd809f39dbe12ff5e17cd
Dan Popovic <dpopovic@tribalscale.com>
2022-06-26 13:51:41 -0400

    chore: add simple pipe test

--- __tests__/pipe.test.ts
+++ __tests__/pipe.test.ts
@@ -7,6 +7,13 @@ import { asyncPipe } from '../src/utils/pipe'
 import { composeSuccessResponse } from '../src/utils/responses'
 
 describe('asyncPipe unit test', () => {
+  it('should invoke fn 3 times to add 5 to initial value', async () => {
+    const fn = (a: number): number => a + 5
+    const initalValue = 0
+    const actual = await asyncPipe(fn, fn, fn)(initalValue)
+    const expected = 15
+    expect(actual).toEqual(expected)
+  })
   it('should format album collection and return in lambda response', async () => {
     const actual = await asyncPipe(
       formatAlbumCollection,
